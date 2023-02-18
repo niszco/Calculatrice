6d74142506f4b95a3d8a3d8ed9d48ee04331bdd0
niszco <nico.marszalek2@gmail.com>
2023-02-18 19:58:23 +0100

    Minor changes + js install

---
+++ main.js
@@ -0,0 +1,17 @@
+// Main functions for basic operations.
+
+function addition(a, b){
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
--- main.ts
+++
@@ -1,7 +0,0 @@
-function addition(a: number, b: number): number {
-  return a + b;
-}
-
-function multiply(a: number, b: number): number {
-  return a * b;
-}
