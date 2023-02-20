1e8e913444886db8943c99df3e0caa3a05ee7ff4
niszco <nico.marszalek2@gmail.com>
2023-02-20 17:17:39 +0100

    Fix js, fix css and minor changes html

--- main.js
+++ main.js
@@ -1,10 +1,46 @@
-const buttons = document.querySelector("button")
-const display = document.quertySelector("display_numbers");
+"use strict";
 
-console.log(buttons)
+const input = document.getElementById('display_screen');
+const number = document.querySelectorAll('.buttons_num');
+const operations = document.querySelectorAll('.buttons_operations');
+const result = document.getElementById('equals');
+const display = document.getElementById('display_device')
+const cursor = document.getElementById('cursor');
+let resultDisplayed = false;
 
-function calculate(buttons) {
-  console.log(buttons);
+let screenBuffer = [];
+
+function displayScreenCommon(value) {
+  screenBuffer.push(value);
+  display.innerHTML += value;
 }
 
-buttons.forEach(buttons => buttons.addEventListener('click', () => calculate(buttons)))
+function displayScreenOthers(value) {
+  screenBuffer.unshift(value);
+  display.innerHTML += value;
+}
+
+function clearDisplay(value) {
+  display.innerHTML = value;
+}
+
+function displayNumbers(number) {
+  for (let i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", function(x) {
+      let currentString = input.innerHTML;
+      let lastChar = currentString[currentString.length - 1];
+
+      if (resultDisplayed === false) {
+        input.innerHTML += x.target.innerHTML;
+      } else if (resultDisplayed === true && lastChar == '&plus' || lastChar == '&times' || lastChar == '&divide' || lastChar == '-') {
+        resultDisplayed = false;
+        input.innerHTML	+= x.target.innerHTML;
+      } else {
+        resultDisplayed = false;
+        input.innerHTML = "";
+        input.innerHTML += x.target.innerHTML;
+      }
+    });
+
+  }
+}
