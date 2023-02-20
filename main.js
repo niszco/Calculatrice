2560e15cdd12c69376507565498e4c5f09017bb3
niszco <nico.marszalek2@gmail.com>
2023-02-20 12:08:12 +0100

    Minor fixes

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-const device = document.querySelector('.device_frame');
-const buttons = document.querySelector('.device_buttons');
+const buttons = document.querySelector("button")
+const display = document.quertySelector("display_numbers");
 
-buttons.addEventListener('click', x => {
-  if (x.target.matches('device_buttons')) {
-    const buttons = x.target;
-    const action = buttons.dataset.action;
-    const buttonsContent = buttons.textContent;
-    const displayNumber = display.textContent
-  }
-})
+console.log(buttons)
 
-function display(value) {
-  if (value == null) {
-    equals.value += '';
-  } else {
-    equals.value = value;
-  }
+function calculate(buttons) {
+  console.log(buttons);
 }
+
+buttons.forEach(buttons => buttons.addEventListener('click', () => calculate(buttons)))
