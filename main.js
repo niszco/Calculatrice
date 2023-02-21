7fb2754b5babdb4a513add0e702bb6cc2503f5c4
niszco <nico.marszalek2@gmail.com>
2023-02-21 20:51:07 +0100

    Major changes for JS

--- main.js
+++ main.js
@@ -1,57 +1,87 @@
+// Constants and others;
+const calculator = document.querySelector('device_frame');
+const buttons = document.querySelectorAll('button');
 const number = document.querySelectorAll('.buttons_num');
 const operations = document.querySelectorAll('.buttons_operations');
 const result = document.getElementById('equals');
+const clear = document.getElementById('clear');
+const decimal = document.getElementById('float');
 const display = document.getElementById('display_device')
-const cursor = document.getElementById('cursor');
+const audio = new Audio('./click1.wav');
+
+// A flag for knowing when the result is displayed or not;
+let resultDisplayed = false;
+
+// An array to store values that have been previously pressed by the user;
+let screenBuffer = [];
+// Current value of the display;
 const currentValue = display.innerHTML;
+// Last value of the display;
 const lastValue = screenBuffer[screenBuffer.length - 1];
 
-let resultDisplayed = false;
-let screenBuffer = [];
+// Play a sound when any buttons is pressed;
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    audio.play();
+  });
+});
 
+// Main functions for the calculator to work :
+
+// Display numbers in the display;
 function displayScreenCommon(value) {
   screenBuffer.push(value);
   display.innerHTML += value;
 }
 
+// Display operators in the display;
 function displayScreenOthers(value) {
   screenBuffer.unshift(value);
   display.innerHTML += value;
 }
 
-function clearDisplay() {
+// Clear the display;
+clear.addEventListener('click', function() {
   display.innerHTML = '';
+})
+
+// Update the display when the 'equal' key is pressed;
+function updateDisplay(value) {
+  display.innerHTML = value;
 }
 
-function clearOneNumber() {
-  for (let i = 0; i < screenBuffer.length; i++) {
-    if (i === screenBuffer[0]) {
-      return display.innerHTML = '';
-    }
-   else if (i !== screenBuffer[0]) {
-  screenBuffer.pop(value)
-  display.innerHTML = value;
-    }
+// Check if the number is a decimal;
+function decimalBoolean() {
+  if (!display.innerHTML.includes(',')) {
+    display.innerHTML += decimal;
   }
 }
 
+// Result
+result.addEventListener('click', function() {
+  var inputString = display.innerHTML;
+  var numbers = inputString.split(/\+|\-|\*|\÷/g);
+  var operators = inputString.replace(/[0-9]|\,/g,"").split("");
 
-function frac(value) {
-  screenBuffer.unshift(value);
-  display.innerHTML += value;
-}
+  console.log(inputString);
+  console.log(operators);
+  console.log(numbers);
 
-function percent(value) {
-  value = screenBuffer[screenBuffer.length - 1] * (screenBuffer[screenBuffer.length - 1] * 100);
-  display.innerHTML += value;
+for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === "*") {
+      return updateDisplay(numbers[i] *= numbers[i + 1]);
+    }
+    else if (operators[i] === "÷") {
+      return updateDisplay(numbers[i] /= numbers[i + 1]);
+    }
+    else if (operators[i] === "-") {
+      return updateDisplay(numbers[i] -= numbers[i + 1]);
+    }
+    else if (operators[i] === "+") {
+      return updateDisplay(numbers[i] += numbers[i + 1]);
+    }
+  }
 }
-
-function equals(lastValue, currentValue) {
-  
-}
-
-function calcSqr(value) {
-  return Math.sqrt(value);
-}
+)
 
 
