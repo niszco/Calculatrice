928175d448b61c65feb4b00bffb08cab46eccf2e
niszco <nico.marszalek2@gmail.com>
2023-02-23 19:13:31 +0100

    Fix sum JS

--- main.js
+++ main.js
@@ -15,6 +15,11 @@ let resultDisplayed = false;
 // An array to store every values that have been previously pressed by the user;
 let screenBuffer = [];
 
+// Current value of the display;
+const currentValue = display.innerHTML;
+// Last value of the display;
+const lastString = screenBuffer[screenBuffer.length - 1];
+
 // Play a sound when any buttons is pressed;
 buttons.forEach(button => {
   button.addEventListener("click", () => {
@@ -53,13 +58,25 @@ function decimalBoolean() {
   }
 }
 
+function operatorsLimit() {
+  let inputString = display.innerHTML;
+  // Sub array to distinguish numbers and operators from each other;
+  let numbers = inputString.split(/\+|\-|\^|\√|\*|\÷/g);
+  let operators = inputString.replace(/[0-9]|\,/g,"").split("");
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === "*" || operators === "-" || operators === "+" || operators === "/" || operators === "^") {
+
+    }
+  }
+};
+
 // Result
 result.addEventListener('click', function() {
-  var inputString = display.innerHTML;
+  let inputString = display.innerHTML;
   // Sub array to distinguish numbers and operators from each other;
-  var numbers = inputString.split(/\+|\-|\^|\√|\*|\÷/g);
-  var operators = inputString.replace(/[0-9]|\,/g,"").split("");
-
+  let numbers = inputString.split(/\+|\-|\^|\√|\*|\÷/g);
+  let operators = inputString.replace(/[0-9]|\,/g,"").split("");
+  
   console.log(inputString);
   console.log(operators);
   console.log(numbers);
@@ -68,21 +85,27 @@ result.addEventListener('click', function() {
 for (let i = 0; i < numbers.length; i++) {
 // Every operator is tested to see if it matches the current operator displayed;
     if (operators[i] === "*") {
+      resultDisplayed = true;
       return updateDisplay(numbers[i] *= numbers[i + 1]);
     }
     else if (operators[i] === "÷") {
+      resultDisplayed = true;
       return updateDisplay(numbers[i] /= numbers[i + 1]);
     }
     else if (operators[i] === "-") {
+      resultDisplayed = true;
       return updateDisplay(numbers[i] -= numbers[i + 1]);
     }
     else if (operators[i] === "+") {
-      return updateDisplay(numbers.reduce((a, b) => a += b));
+      resultDisplayed = true;
+      return updateDisplay(parseFloat(numbers[i], 10) + parseFloat(numbers[i + 1], 10));
     }
     else if (operators[i] === "^") {
+      resultDisplayed = true;
       return updateDisplay(numbers[i] ** numbers[i + 1]);
     }
     else if (operators[i] === "√") {
+      resultDisplayed = true;
       return updateDisplay(Math.sqrt(numbers[i]));
     }
   }
